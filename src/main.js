5198fce62ede11d13a89af1cc774abe110a137f2
tanwenchaoya <1781104182@qq.com>
2020-04-13 11:22:45 +0800

    初始化项目

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import fastclick from 'fastclick'
+import './assets/css/base.scss'
+fastclick.attach(document.body)
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
